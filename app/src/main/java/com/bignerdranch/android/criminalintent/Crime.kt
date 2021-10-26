2f8eca739dbd409ca078ad04bb9f372338f508e7
tomdora <47201507+tomdora@users.noreply.github.com>
2021-10-26 15:27:56 -0400

    UI Changes, updating to allow for a local database of crimes

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-class Crime(val id: UUID = UUID.randomUUID(),
-            var title: String = "",
-            var date: Date = Date(),
-            var isSolved: Boolean = false) {
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false) {
 
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -47,9 +49,31 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.adapter = adapter
     }
 
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
-        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var crime: Crime
+
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        override fun onClick(v: View) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
@@ -62,10 +86,7 @@ class CrimeListFragment : Fragment() {
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)
         }
     }
 
